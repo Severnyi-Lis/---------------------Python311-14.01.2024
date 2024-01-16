5b755342509821672961bb55fa6d658bdf67a6c3
Severnyi-Lis <xnestro@mail.ru>
2024-01-16 04:33:33 +0300

    добавил недостающие точки с запятой

--- task.js
+++ task.js
@@ -6,42 +6,42 @@ function chs(тык){
     тык.currentTarget.style.color = 'red';
     pol_inf.style.color='blue';
     pol_inf_2.style.color='red';
-    console.log(ttt,pol_inf,pol_inf_2)
+    console.log(ttt,pol_inf,pol_inf_2);
 }
 function one(тыгыдык){
     тыгыдык.currentTarget.style.backgroundColor='white';
     soup_1.style.backgroundColor='violet';
-    console.log(nam,soup_1)
+    console.log(nam,soup_1);
 }
 function two(hhh){
     hhh.currentTarget.style.backgroundColor='white'
     soup_2.style.backgroundColor='green';
-    console.log(surnam,soup_2)
+    console.log(surnam,soup_2);
 }
 function three(lst){
     lst.currentTarget.style.backgroundColor='white';
     soup_3.style.backgroundColor='blue';
-    console.log(middle_nam,soup_3)
+    console.log(middle_nam,soup_3);
 }
 function four(мурмур){
     мурмур.currentTarget.style.backgroundColor='white';
     soup_4.style.backgroundColor='magenta';
-    console.log(born,soup_4)
+    console.log(born,soup_4);
 }
 function five(сыыыр){
     сыыыр.currentTarget.style.backgroundColor='white';
     soup_5.style.backgroundColor='red';
-    console.log(log,soup_5)
+    console.log(log,soup_5);
 }
 function six(bro){
     bro.currentTarget.style.backgroundColor='white';
     soup_6.style.backgroundColor='royalblue';
-    console.log(post,soup_6)
+    console.log(post,soup_6);
 }
 function seven(апчих){
     апчих.currentTarget.style.backgroundColor='white';
     soup_7.style.backgroundColor='lime';
-    console.log(log_2,soup_7)
+    console.log(log_2,soup_7);
 }
 function kuk(event_start){
     ttt.addEventListener(
